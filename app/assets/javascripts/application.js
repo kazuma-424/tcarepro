c0040ea3317b26d2eeb2be042068ed8eb9e38962
kenta-crv <okuyamakenta@gmail.com>
2021-01-27 21:41:16 +0900

    fix

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -101,3 +101,76 @@ $(function() {
       return pr_code
     }
   });
+
+  id_checkes = function(ele){
+    ids = $("input[type='checkbox'].company-checkbox").filter(":checked").map(function(){
+      return $(this).attr("id");
+    }).get();
+    $("#company-destroy-link").attr("href", "/companies/bulk_destroy?" + $.param({"ids":ids}));
+    mails = $("input[type='checkbox'].company-checkbox").filter(":checked").map(function(){
+      return $(this).data()["email"];
+    }).get();
+    var params = $("#company-mail-link").attr("href").split("?")[1]
+    $("#company-mail-link").attr("href", "mailto:" + mails.join(",") + "?" + params);
+    // $("#company-mail-link").attr("href", "/companies/mail?" + $.param({"mails":mails}));
+    comment_ids = $("input[type='checkbox'].company-checkbox").filter(":checked").map(function(){
+      return $(this).data()["commentId"];
+    }).get();
+    $("#comment-destroy-link").attr("href", "/comments/bulk_destroy?" + $.param({"ids":comment_ids}));
+    $("#comment-uploads-link").attr("href", "/comments/bulk_edit?" + $.param({"ids":comment_ids}));
+
+
+  }
+
+
+  //takigawa
+  $(document).on('turbolinks:load', function(event) {
+  //$(document).on('ready', function(event) {
+          //company/:id
+      $(document).on("click", ".edit-link-show", function () {
+        data = $(this).data()
+        $("#" + data.v + "-show").addClass("none")
+        $("#" + data.v + "-c-input-field").removeClass("none")
+      });
+      $(".c-input-field").keypress( function ( e ) {
+      	if ( e.which == 13 ) {
+          x = $('#company-show-update').submit();
+      	}
+      } );
+      $(".c-input-field2").keypress( function ( e ) {
+      	if ( e.which == 13 ) {
+          x = $('#company-show-update2').submit();
+      	}
+      } );
+      $(document).on("keypress", ".t-input-field", function (e) {
+        if ( e.which == 13 ) {
+          x = $('#todo-show-update').submit();
+      	}
+      });
+      $(document).on("click", ".comment-edit", function () {
+        data = $(this).data()
+        $("#" + data.id + "-comment-body").toggle();
+        $("#" + data.id + "-comment-edit-field").toggle();
+      })
+
+      $(document).on("click", ".comment-edit-detail", function () {
+        data = $(this).data()
+        $("#" + data.id + "-comment-edit-detail-field").toggle();
+      })
+
+      $(document).on("change", "#form-bulk-edit", function () {
+        $(".form-bulk-edit-input").addClass("none")
+        v = $(this).val()
+        $("#form-" + v).removeClass("none")
+      })
+
+
+
+      $("#company-index-table").tablesorter();
+      $(".company-checkbox").change(function(){
+        id_checkes()
+      });
+      $(".all-company-checkbox").click(function(){
+        $('.admin-checkbox').prop('checked', this.checked);
+        id_checkes()
+      });
