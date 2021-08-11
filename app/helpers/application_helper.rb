5172c8b11c6f6c474b5c291748c2403b3c90429b
kenta-crv <okuyamakenta@gmail.com>
2021-08-11 14:41:59 +0900

    fix

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,9 @@
 module ApplicationHelper
   def default_meta_tags
     {
-      site: "営業で最高の結果を出せるマーケティングリストなら『TCARE』",
+      site: "営業リストで最高の結果を出すならマーケティングリストの『TCARE』",
       title:"<%= yield(:title) | TCARE' %>",
-      description: "営業で最高の結果を出せるマーケティングリストなら『TCARE』。『イマ』ニーズのある企業情報を提供します。",
-      keywords: "営業リスト,マーケティングリスト",
+      description: "営業リストで最高の結果を出すならマーケティングリストの『TCARE』。マーケティングで成功する営業リストを提供します。",
       canonical: request.original_url,  # 優先されるurl
       charset: "UTF-8",
       reverse: true,
