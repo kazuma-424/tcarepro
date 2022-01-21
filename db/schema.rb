738781aa03c13288e458837189baa4367598e38a
kenta-crv <okuyamakenta@gmail.com>
2022-01-21 17:09:09 +0900

    fix

--- db/schema.rb
+++ db/schema.rb
@@ -16,12 +16,12 @@ ActiveRecord::Schema.define(version: 20220114212658) do
     t.string "user_name", default: "", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "select"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "select"
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
   end
@@ -34,10 +34,8 @@ ActiveRecord::Schema.define(version: 20220114212658) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "admin_id"
-    t.integer "crm_id"
     t.integer "user_id"
     t.index ["admin_id"], name: "index_calls_on_admin_id"
-    t.index ["crm_id"], name: "index_calls_on_crm_id"
     t.index ["customer_id", "created_at"], name: "index_calls_on_customer_id_and_created_at"
     t.index ["customer_id"], name: "index_calls_on_customer_id"
     t.index ["user_id"], name: "index_calls_on_user_id"
@@ -89,36 +87,6 @@ ActiveRecord::Schema.define(version: 20220114212658) do
     t.index ["sender_id"], name: "index_counts_on_sender_id"
   end
 
-  create_table "crms", force: :cascade do |t|
-    t.string "company"
-    t.string "first_name"
-    t.string "last_name"
-    t.string "first_kana"
-    t.string "last_kana"
-    t.string "tel"
-    t.string "mobile"
-    t.string "fax"
-    t.string "mail"
-    t.string "postnumber"
-    t.string "prefecture"
-    t.string "city"
-    t.string "town"
-    t.string "building"
-    t.string "item"
-    t.string "statu"
-    t.string "price"
-    t.string "number"
-    t.string "history"
-    t.string "area"
-    t.string "target"
-    t.string "next"
-    t.string "content"
-    t.string "comment"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.datetime "date_time"
-  end
-
   create_table "customers", force: :cascade do |t|
     t.string "company"
     t.string "store"
@@ -141,7 +109,7 @@ ActiveRecord::Schema.define(version: 20220114212658) do
     t.string "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "memo_5"
+    t.string "choice"
     t.string "title"
     t.string "other"
     t.string "url_2"
@@ -153,8 +121,6 @@ ActiveRecord::Schema.define(version: 20220114212658) do
     t.string "area"
     t.string "target"
     t.date "start"
-    t.string "choice"
-    t.string "sfa_statu"
     t.string "contact_url"
     t.string "meeting"
     t.string "experience"
@@ -163,15 +129,6 @@ ActiveRecord::Schema.define(version: 20220114212658) do
     t.integer "worker_id"
     t.index ["created_at"], name: "index_customers_on_created_at"
     t.index ["worker_id"], name: "index_customers_on_worker_id"
-  end
-
-  create_table "customers_search_orders", force: :cascade do |t|
-    t.integer "admin_id"
-    t.integer "customer_id"
-    t.integer "prev_customer_id"
-    t.integer "next_customer_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "estimates", force: :cascade do |t|
@@ -211,17 +168,11 @@ ActiveRecord::Schema.define(version: 20220114212658) do
     t.index ["crm_id"], name: "index_images_on_crm_id"
   end
 
-<<<<<<< HEAD
-  create_table "industries", force: :cascade do |t|
-    t.string "key", null: false
-    t.integer "incentive"
-=======
   create_table "incentives", force: :cascade do |t|
     t.string "customer_summary_key", null: false
     t.integer "year", null: false
     t.integer "month", null: false
     t.integer "value", null: false
->>>>>>> 4d67ecb5fe840ae2f501da2b06c71a9b58ba5cde
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["customer_summary_key", "year", "month"], name: "index_incentives_on_customer_summary_key_and_year_and_month"
@@ -273,7 +224,6 @@ ActiveRecord::Schema.define(version: 20220114212658) do
     t.string "user_name", default: "", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "select"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -281,20 +231,6 @@ ActiveRecord::Schema.define(version: 20220114212658) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_senders_on_email", unique: true
     t.index ["reset_password_token"], name: "index_senders_on_reset_password_token", unique: true
-  end
-
-  create_table "skillsheets", force: :cascade do |t|
-    t.string "name"
-    t.string "tel"
-    t.string "mail"
-    t.string "address"
-    t.string "age"
-    t.string "start"
-    t.string "experience"
-    t.string "history"
-    t.string "title"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "smartphone_logs", force: :cascade do |t|
@@ -307,22 +243,6 @@ ActiveRecord::Schema.define(version: 20220114212658) do
     t.string "device_name", null: false
     t.string "token", null: false
     t.boolean "delete_flag", default: false, null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "upload_data", force: :cascade do |t|
-    t.string "name"
-    t.string "file"
-    t.integer "company_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["company_id"], name: "index_upload_data_on_company_id"
-  end
-
-  create_table "upload_files", force: :cascade do |t|
-    t.string "name"
-    t.string "file"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
