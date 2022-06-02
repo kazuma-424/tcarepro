3c1979ec5c61705feb6e5316de0814d54c3b575d
kenta-crv <okuyamakenta@gmail.com>
2022-06-02 17:53:26 +0900

    analy fix

--- app/controllers/workers_controller.rb
+++ app/controllers/workers_controller.rb
@@ -7,12 +7,14 @@ class WorkersController < ApplicationController
     @count_month = @customers.where('updated_at > ?', Time.current.beginning_of_month).where('updated_at < ?',Time.current.end_of_month).count
 
     @contact_trackings_month = @worker.contact_trackings.where(created_at: Time.current.beginning_of_month..Time.current.end_of_month)
+    @contact_trackings_before_month = @worker.contact_trackings.where(created_at: 1.month.ago.beginning_of_month..1.month.ago.end_of_month)
     @contact_trackings_day = @worker.contact_trackings.where(created_at: Time.current.beginning_of_day..Time.current.end_of_day)
     @contact_trackings_week = @worker.contact_trackings.where(created_at: Time.current.beginning_of_week..Time.current.end_of_week)
 
     @send_success_count_day = @contact_trackings_day.where(status: '送信済').count.to_i
     @send_success_count_week = @contact_trackings_week.where(status: '送信済').count.to_i
     @send_success_count_month = @contact_trackings_month.where(status: '送信済').count.to_i
+    @send_success_count_before_month = @contact_trackings_before_month.where(status: '送信済').count.to_i
 
     @send_error_count_month = @contact_trackings_month.where(status: '送信不可').count.to_i
     @send_ng_count_month = @contact_trackings_month.where(status: '営業NG').count.to_i
