f068405e6963a7d2dcee1bb8c0d84a5f79b940cb
kenta-crv <okuyamakenta@gmail.com>
2023-01-15 14:44:18 +0900

    fix

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20230114082655) do
+ActiveRecord::Schema.define(version: 20230115054236) do
 
   create_table "admins", force: :cascade do |t|
     t.string "user_name", default: "", null: false
